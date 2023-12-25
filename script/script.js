4b180bd52b791d8bfa1debb43de3c3d2981df87b
mohamedemam4078 <mohamedemam4078@gmail.com>
2023-12-26 01:55:28 +0200

    Add files via upload

--- script/script.js
+++
@@ -1,7 +0,0 @@
-url = "https://api.ipify.org/?format=json" ;
-var ip = document.getElementById("found");
-function find(){
-    fetch(url).then(res => res.json().then(result =>  {
-        ip.innerText = result.ip;
-    }))
-}
