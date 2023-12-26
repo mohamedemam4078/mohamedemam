8fa9f0de18891af2fc2f107071230ed9cb774b1c
mohamedemam4078 <mohamedemam4078@gmail.com>
2023-12-26 12:41:26 +0200

    Add files via upload

--- js/resume_download.js
+++ js/resume_download.js
@@ -2,7 +2,7 @@ function downloadPDF() {
     // Create a link element
     var link = document.createElement('a');
     link.href = '../Resume.pdf';
-    link.download = 'Mohamed Emam Resume.pdf'; // Set the desired file name
+    link.download = 'Mohamed_Emam_Resume.pdf'; // Set the desired file name
 
     // Simulate a click event to trigger the download
     if (document.createEvent) {
